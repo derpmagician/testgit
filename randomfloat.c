2831f765dc1194b9970a136c59f1620c23fb3445
Renzo Perez <renzin@gmail.com>
2021-08-29 17:26:22 -0500

    test from vscode

---
+++ randomfloat.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive", n);
+	else if (n < 0)
+		printf("%d is negaive", n);
+	else
+		printf("%d is zero", n);
+	printf("\n");
+
+	return (0);
+}
